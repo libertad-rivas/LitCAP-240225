3549819054bcb74c08a54a7c313f2c345b2e96bd
Libertad Rivas <lidia.rivas@axity.com>
2025-02-24 21:45:57 -0600

    agregando archivos vistos en clase

--- Leccion_3/src/app-header.ts
+++ Leccion_3/src/app-header.ts
@@ -2,24 +2,20 @@ import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 
 @customElement("app-header")
-class AppHeader extends LitElement{
-
-    static styles = css`
-    
-    h1{
-        font-family: sans-serif, "Times New Roman";
-        font-weight: bold;
-        text-align: center;
+class AppHeader extends LitElement {
+  static styles = css`
+    h1 {
+      font-family: sans-serif, "Times New Roman";
+      font-weight: bold;
+      text-align: center;
     }
+  `;
+
+  render() {
+    return html`
+      <header>
+        <h1>Cabecera</h1>
+      </header>
     `;
-
-    
-    render() {
-        return html`
-        <header>
-            <h1> Cabecera</h1>
-        </header>
-        
-        `;
-    }
+  }
 }
--- Leccion_3/src/app-page.ts
+++ Leccion_3/src/app-page.ts
@@ -1,17 +1,16 @@
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
+import "./app-header.ts";
+import "./app-article.ts";
+import "./app-footer.ts";
 
 @customElement("app-page")
-class AppPage extends LitElement{
-
-    static styles = css``;
-
-    render() {
-        return html`
-            <app-header></app-header>
-            <app-article></app-article>
-            <app-footer></app-footer>
-
-        `;
-    }
+class AppPage extends LitElement {
+  render() {
+    return html`
+      <app-header></app-header>
+      <app-article></app-article>
+      <app-footer></app-footer>
+    `;
+  }
 }
