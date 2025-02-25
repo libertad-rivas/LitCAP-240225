2745c54ed38ca66f1329524820e5d59fb843e557
Libertad Rivas <lidia.rivas@axity.com>
2025-02-25 16:28:05 -0600

    Se crea página HOME donde solo se renderiza de otros archivos

--- Ejercicio_final/src/home-page.ts
+++ Ejercicio_final/src/home-page.ts
@@ -1,4 +1,4 @@
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 import "./header-page.ts";
 import "./footer-page.ts";
